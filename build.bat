520b3e6337c7863defb9fe2cb84f8c58cec53060
Minpa Lee <mapplus@gmail.com>
2013-07-29 00:47:38 +0900

    initial commit

---
+++ build.bat
@@ -0,0 +1,25 @@
+cd gt-ngi
+mvn clean install
+mvn package
+cd ..
+
+cd jdbc-altibase
+mvn clean install
+mvn package
+cd ..
+
+cd jdbc-kairos
+mvn clean install
+mvn package
+cd ..
+
+cd jdbc-pgdb
+mvn clean install
+mvn package
+cd ..
+
+cd jdbc-tibero
+mvn clean install
+mvn package
+cd ..
+
